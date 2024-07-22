a0cb6b2a9d820b8ef846b712943d95619fe013ec
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2024-07-21 20:43:23 -0500

    creacion del proyecto

---
+++ src/main/java/com/co/pest_tech/labs/ptl_admin_ms_usuarios/PtlAdminMsUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.co.pest_tech.labs.ptl_admin_ms_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtlAdminMsUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtlAdminMsUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/pest_tech/labs/ptl_admin_ms_usuarios/PtlAdminMsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.pest_tech.labs.ptl_admin_ms_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtlAdminMsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
